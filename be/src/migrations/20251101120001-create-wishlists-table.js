b1e06c71d85659f7eb9af324f83a36138d2cc0d4
AnLangThang81 <nguyenhoaian2821@gmail.com>
2025-11-18 23:00:39 +0700

    feature: add migration for brands and wishlists

---
+++ be/src/migrations/20251101120001-create-wishlists-table.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('wishlists', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+      },
+      product_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+
+    // Add unique index on user_id and product_id
+    await queryInterface.sequelize.query('CREATE UNIQUE INDEX IF NOT EXISTS wishlists_user_id_product_id ON wishlists (user_id, product_id);');
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('wishlists');
+  },
+};
